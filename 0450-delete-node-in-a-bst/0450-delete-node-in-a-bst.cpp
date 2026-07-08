e5076f59169a6d5a09840d303f0b2488376a5d5e
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-07-09 02:52:07 +0530

    Time: 0 ms (100%), Space: 34.5 MB (15.56%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    //helper
+    TreeNode* minValueNode(TreeNode* node) {
+        TreeNode* current = node;
+        while (current && current->left != nullptr) {
+            current = current->left;
+        }
+        return current;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+         if(root == nullptr) return root;
+
+        if(key< root->val){
+            root->left = deleteNode(root->left,key);   
+        }else if(key > root->val){
+            root->right = deleteNode(root->right,key);
+        }
+        else{
+            if(root->left==nullptr){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }else if(root->right == nullptr){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+        
+
+        //node has 2 children ->preorder successor assign kro(lowest in right)
+        TreeNode* temp = minValueNode(root->right);
+        root->val = temp->val;
+        //delete it
+        root->right = deleteNode(root->right,temp->val);
+        
+    }
+    return root;
+
+    }
+};
