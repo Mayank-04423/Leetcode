0cbd5882383eaf207f722f5fd3e35e30da0108e3
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-08-04 01:22:46 +0530

    Time: 0 ms (100%), Space: 18.7 MB (12.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,20 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-        bool checkMirror(TreeNode*p,TreeNode*q){   
-        //empty 
-        if(p ==nullptr && q ==nullptr) return true;
-        //either one empty
-        if(p==nullptr || q==nullptr)return false;
-        //exist but non equal values
-        if(p->val != q->val) return false;
-
-        //matching left and right subtrees
-        return checkMirror(p->left,q->right) && checkMirror(p->right,q->left);
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return false;
+        return isMirror(root->left , root->right);
     }
 
-    bool isSymmetric(TreeNode* root) {
-       if(root == nullptr) return true;
-
-       return checkMirror(root->left, root->right);
+    bool isMirror(TreeNode* left , TreeNode* right){
+        if(!left && !right) return true;
+        if(!right || !left) return false;
+        
+        return (left->val == right->val) &&
+            isMirror(left->left,right->right)&&
+            isMirror(left->right, right->left);
     }
 };
