8b405dd772aa88568351c10dbc673340e53b741a
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-08-10 01:43:34 +0530

    Time: 20 ms (94.01%), Space: 16.3 MB (80.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    void dfs(vector<vector<char>>& grid, int i, int j){
-            if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0')
-            return ;
+    void bfs(vector<vector<char>>& grid,int i, int j){
+        if(i<0|| i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0')
+        return;
 
-            grid[i][j]='0';
-
-            dfs(grid, i, j+1);
-            dfs(grid, i, j-1);
-            dfs(grid, i+1, j);
-            dfs(grid, i-1, j);
+        grid[i][j]='0';
+        
+        bfs(grid , i , j+1);
+        bfs(grid , i , j-1);
+        bfs(grid , i+1 , j);
+        bfs(grid , i-1 , j);
         }
+   
 public:
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
@@ -17,11 +18,10 @@ public:
             for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                 }
             }
         }
-        return count;        
-
+        return count;
     }
 };
