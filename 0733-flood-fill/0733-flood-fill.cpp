c2077e7870b1d3a3eb9edd23bd09a43125d1f9e0
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-08-10 01:54:09 +0530

    Time: 0 ms (100%), Space: 18.1 MB (75.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    void dfs(vector<vector<int>>& image, int i, int j, int oldColor, int newColor) {
-        if (i < 0 || i >= image.size() || j < 0 || j >= image[0].size() || image[i][j] != oldColor)
-            return;
+    void bfs(vector<vector<int>>& image , int i, int j,int oldColor , int color){
+        if(i<0 || i>=image.size() || j<0 || j>image[0].size() || image[i][j]!=oldColor)
+        return;
 
-        image[i][j] = newColor;
+        image[i][j]= color;
 
-        dfs(image, i + 1, j, oldColor, newColor);
-        dfs(image, i - 1, j, oldColor, newColor);
-        dfs(image, i, j + 1, oldColor, newColor);
-        dfs(image, i, j - 1, oldColor, newColor);
+        bfs(image, i , j+1, oldColor , color);
+        bfs(image, i , j-1, oldColor , color);
+        bfs(image, i+1 , j, oldColor , color);
+        bfs(image, i-1 , j, oldColor , color);        
+
     }
-
+    
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int oldColor = image[sr][sc];
-        if (oldColor == color) return image;
+        if(oldColor==color)return image ;
 
-        dfs(image, sr, sc, oldColor, color);  
+        bfs(image, sr ,sc ,oldColor, color);
         return image;
-    }
+    }    
 };
