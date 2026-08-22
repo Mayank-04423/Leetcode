e778574c55c3cb99e389c3bb72e4dc5e210a3e2c
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-08-22 21:28:00 +0530

    Time: 12 ms (41.29%), Space: 65 MB (69.91%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,20 +3,18 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow = nums[0];
         int fast = nums[0];
-
+        
         do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+       }
+       while(slow!=fast);
+
+        fast = nums[0];
+        while(fast!= slow){
             slow = nums[slow];
-            fast = nums[nums[fast]];
-        }while(slow!=fast);
-
-        //locating the cycle
-        int ptr1 = nums[0];
-        int ptr2 = slow;
-
-        while(ptr1!=ptr2){
-            ptr1=nums[ptr1];
-            ptr2=nums[ptr2];
-        }
-        return ptr1;
+            fast = nums[fast];
+        }       
+        return slow;
     }
 };
