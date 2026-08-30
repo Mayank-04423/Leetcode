d7532b26dc12d138ac300d52d1aef472e3340d45
Mayank Tiwari <mayanktiwari0026@gmail.com>
2026-08-30 21:26:21 +0530

    Time: 1 ms (25.11%), Space: 71.7 MB (52.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,11 +4,11 @@ public:
         int best = nums[0];
         int ans = nums[0];
 
-        for(int i=1; i<nums.size();i++){
-            int v1 = nums[i]+best;
+        for(int i=1; i<nums.size(); i++){
+            int v1 = best + nums[i];
             int v2 = nums[i];
             best = max(v1,v2);
-            ans = max(best, ans);
+            ans = max(ans,best);
         }
         return ans;
     }
